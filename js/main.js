d193cb90029d940c9c8be9d55c3bdea7a37fede1
Lee <barneslee39@gmail.com>
2022-08-24 16:02:15 +0100

    Updates

--- js/main.js
+++ js/main.js
@@ -18,6 +18,10 @@
     var paragraph5 = document.getElementById('paragraph5');
     var paragraph6 = document.getElementById('paragraph6');
 
+    var marble = document.getElementById('marble');
+    var pencil = document.getElementById('pencil');
+    var li = document.getElementById('li');
+
     function changeColorWhite() {
         title.style.color = '#ffffff';
         title2.style.color = '#ffffff';
@@ -28,6 +32,10 @@
         paragraph4.style.color = '#ffffff';
         paragraph5.style.color = '#ffffff';
         paragraph6.style.color = '#ffffff';
+
+        marble.src = 'img/marble.png';
+        pencil.src = 'img/pencil.png';
+        li.src = 'img/linkedin.png';
     }
 
     function changeColorYellow() {
@@ -40,6 +48,10 @@
         paragraph4.style.color = '#ffde59';
         paragraph5.style.color = '#ffde59';
         paragraph6.style.color = '#ffde59';
+
+        marble.src = 'img/marble.png';
+        pencil.src = 'img/pencil3.png';
+        li.src = 'img/linkedin3.png';
     }
 
     function changeColorPink() {
@@ -52,4 +64,8 @@
         paragraph4.style.color = '#e4138f';
         paragraph5.style.color = '#e4138f';
         paragraph6.style.color = '#e4138f';
+
+        marble.src = 'img/marble.png';
+        pencil.src = 'img/pencil2.png';
+        li.src = 'img/linkedin2.png';
     }
